d7121e96b6da9834b665a29070a75d04f584bf7c
Melissa Engberg <melissa.j.engberg@gmail.com>
2023-02-21 10:48:44 +0100

    Seems I have to commit this as well for the images?

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -12,13 +12,14 @@ function Game() {
     const playChoice = useCallback((selected) => {
         setChoice(selected);
     }, [])
-
-
-
+ 
     return(
         <>
         <div className="grid-container">
+            <div>
+                <img src={require("./images/rock-hand.png")}/>
             <button className="choice-button" onClick={() => playChoice("rock")}>Rock</button>
+            </div>
             <button className="choice-button" onClick={() => playChoice("paper")}>Paper</button>
             <button className="choice-button" onClick={() => playChoice("scissor")}>Scissor</button>
         </div>
